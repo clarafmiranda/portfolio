6a2b6359f0a6d56be789502c880c7c5e5df564d2
clarafmiranda <41785298+clarafmiranda@users.noreply.github.com>
2019-06-13 12:16:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("navbar");
+function myFunction() {
+  if (menu.style.display === "none") {
+    menu.style.display = "initial";
+  } else {
+    menu.style.display = "none";
+  }
+
+}
